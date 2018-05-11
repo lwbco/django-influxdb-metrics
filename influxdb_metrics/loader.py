20d2396a2a7a88bf170a81d70e7948c9abaa6015
Leeward Bound <l@lwb.co>
2018-05-11 13:36:02 -0400

    Adding entrypoint logging

--- influxdb_metrics/loader.py
+++ influxdb_metrics/loader.py
@@ -13,3 +13,15 @@ if getattr(settings, 'INFLUXDB_USE_CELERY', False):
     write_points = write_points_celery.delay
 else:
     write_points = write_points_normal
+
+def log_startup():
+    from .utils import build_tags
+    data = [{
+        'measurement': 'django_entrypoint',
+        'tags': build_tags(),
+        'fields': {'value': 1, },
+    }]
+    write_points(data)
+
+if getattr(settings, 'INFLUXDB_LOG_ENTRYPOINT', True):
+    log_startup()
